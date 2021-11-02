20fd74ea995543d4e452182cd755aa278ec31924
grant <trainfantn@live.com>
2021-11-01 23:59:09 -0400

    started work on web project

---
+++ web-project/src/js/index.js
@@ -0,0 +1,12 @@
+/*
+const mobileNav = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_ank');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    })
+}
+
+mobileNav();
+*/
